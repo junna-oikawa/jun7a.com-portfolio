1c07ef2a0eccb43127391fd18634ee50c1e01c1f
junna-oikawa <jnn.y326@gmail.com>
2022-09-14 18:57:22 +0900

    [add]ベースを作成

---
+++ src/components/templates/Interests/index.tsx
@@ -0,0 +1,14 @@
+import Style from './style.module.scss';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const InterestsComponent: React.FC = () => {
+  return (
+    <>
+      <p>interests</p>
+      <p>コンポーネント</p>
+    </>
+  );
+};
+
+export default InterestsComponent;
