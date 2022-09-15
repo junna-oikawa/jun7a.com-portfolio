bbc5ec561f5f8fa151f3137e66faa07e4bd6b2b4
junna-oikawa <jnn.y326@gmail.com>
2022-09-15 22:50:15 +0900

    [add]mv-sp

---
+++ src/components/organisms/common/footer_nav/index.tsx
@@ -0,0 +1,10 @@
+import Style from './style.module.scss';
+const FooterNav: React.FC = () => {
+  return (
+    <>
+      <div className={Style.wrapper}></div>
+    </>
+  );
+};
+
+export default FooterNav;
--- src/components/templates/index/index.tsx
+++ src/components/templates/index/index.tsx
@@ -5,6 +5,7 @@ import Header from 'components/organisms/common/header';
 import Works from 'components/organisms/index/works';
 import About from 'components/organisms/index/about';
 import Footer from 'components/organisms/common/footer';
+import FooterNav from 'components/organisms/common/footer_nav';
 
 const Top: React.FC = () => {
   return (
@@ -13,14 +14,17 @@ const Top: React.FC = () => {
       <header className={Style.header}>
         <Header />
       </header>
-      {/* <main className={Style.main}>
+      <main className={Style.main}>
         <MV />
-        <Works />
-        <About />
+        {/* <Works />
+        <About /> */}
       </main>
-      <footer className={Style.footer}>
+      {/* <footer className={Style.footer}>
         <Footer />
       </footer> */}
+      <div className={Style.footer_nav}>
+        <FooterNav />
+      </div>
     </>
   );
 };
