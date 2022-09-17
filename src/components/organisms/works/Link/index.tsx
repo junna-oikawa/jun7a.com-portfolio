314cf3482ee6276a7cad0db87e73b85099e59ea3
junna-oikawa <jnn.y326@gmail.com>
2022-09-17 11:14:04 +0900

    [fix] works-hatome-url フォントの調整

--- src/components/organisms/works/Link/index.tsx
+++ src/components/organisms/works/Link/index.tsx
@@ -10,7 +10,7 @@ const Link: React.FC<Props> = ({ href }: Props) => {
     <>
       <Layout heading='App URL' kana='アプリURL'>
         <div className={Style.wrapper}>
-          <a href={href} target='_brank'>
+          <a href={href} target='_brank' className='en'>
             {href}
             <img src='/images/works/common/open_in_new.svg' alt='別タブでアプリを開く' />
           </a>
