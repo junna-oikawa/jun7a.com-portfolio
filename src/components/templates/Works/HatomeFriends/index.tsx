59937d451b178d5aec476a816f78a9321f613d51
junna-oikawa <jnn.y326@gmail.com>
2022-09-17 11:11:50 +0900

    [create]works-hatome-url

--- src/components/templates/Works/HatomeFriends/index.tsx
+++ src/components/templates/Works/HatomeFriends/index.tsx
@@ -4,11 +4,13 @@ import Mv from 'components/organisms/works/hatomeFriends/Mv';
 import Visuals from 'components/organisms/works/hatomeFriends/Visuals';
 import DescriptionEyelet from 'components/organisms/works/hatomeFriends/DescriptionEyelet';
 import DescriptionApp from 'components/organisms/works/hatomeFriends/DescriptionApp';
+import Link from 'components/organisms/works/Link';
 
 const HatomeFriendsComponent: React.FC = () => {
   return (
     <Layout>
       <Mv />
+      <Link href='https://hatome-friends.herokuapp.com/' />
       <Visuals />
       <DescriptionEyelet />
       <DescriptionApp />
