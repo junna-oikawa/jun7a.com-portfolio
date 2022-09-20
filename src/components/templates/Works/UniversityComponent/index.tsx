92646ddb270ae0cfc5ffbf0ab6d5fcf2f2d8c865
junna-oikawa <jnn.y326@gmail.com>
2022-09-20 20:45:43 +0900

    [create]works-uni-target

--- src/components/templates/Works/UniversityComponent/index.tsx
+++ src/components/templates/Works/UniversityComponent/index.tsx
@@ -5,6 +5,7 @@ import Visuals from 'components/organisms/works/University/Visuals';
 import Layout from 'components/templates/layout';
 import BgPurposeLayout from 'components/organisms/works/common/BgPurposeLayout';
 import BgPurpose from 'components/organisms/works/University/BgPurpose';
+import Target from 'components/organisms/works/University/Target';
 
 const UniversityComponent: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const UniversityComponent: React.FC = () => {
       <div className={Style.width}>
         <Link href='https://observablehq.com/@junna-oikawa/second-semester-assignment' />
         <Visuals />
+        <Target />
         <BgPurpose />
       </div>
     </Layout>
