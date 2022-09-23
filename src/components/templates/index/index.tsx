268b901c9d1715123b807c042000040b8869d68d
junna-oikawa <jnn.y326@gmail.com>
2022-09-23 13:44:49 +0900

    [fix] ファイル名修正

--- src/components/templates/index/index.tsx
+++ src/components/templates/index/index.tsx
@@ -1,7 +1,7 @@
 import Layout from '../Layout';
-import About from 'components/organisms/index/tmpAbout';
-import MV from 'components/organisms/index/tmpMv';
-import Works from 'components/organisms/index/tmpWorks';
+import About from 'components/organisms/index/About';
+import MV from 'components/organisms/index/Mv';
+import Works from 'components/organisms/index/Works';
 
 const Top: React.FC = () => {
   return (
