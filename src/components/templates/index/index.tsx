4ec6c922b2f9802fe24bbe40cb68923277892be0
junna-oikawa <43671757+junna-oikawa@users.noreply.github.com>
2022-09-23 22:42:36 +0900

    Revert "PF: トップページ メモリーセクションの作成 (#22)"
    This reverts commit d326d8db48356a0a318a232d7e251dddf592798b.

--- src/components/templates/index/index.tsx
+++ src/components/templates/index/index.tsx
@@ -1,20 +1,17 @@
-import Style from './style.module.scss';
 import Layout from '../Layout';
 import About from 'components/organisms/Top/About';
 import MV from 'components/organisms/Top/Mv';
 import Works from 'components/organisms/Top/Works';
-import Memory from 'components/organisms/Top/Memory';
 
 const Top: React.FC = () => {
   return (
-    <Layout isTopPage>
-      <div className={Style.inner}>
+    <>
+      <Layout isTopPage>
         <MV />
         <Works />
         <About />
-        <Memory />
-      </div>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
