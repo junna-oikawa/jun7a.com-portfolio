b7c812209018b60d95e2bcb8bd2e335f0b955823
junna-oikawa <jnn.y326@gmail.com>
2022-09-23 14:09:01 +0900

    [wip] ファイル名の修正

--- src/components/templates/Works/ForDeliveryMan/index.tsx
+++ src/components/templates/Works/ForDeliveryMan/index.tsx
@@ -1,12 +1,12 @@
 import Style from './style.module.scss';
 import Layout from 'components/templates/Layout';
-import Mv from 'components/organisms/Works/ForDeliveryMan/Mv';
-import Visuals from 'components/organisms/Works/ForDeliveryMan/Visuals';
-import Target from 'components/organisms/Works/ForDeliveryMan/Target';
-import BgPurpose from 'components/organisms/Works/ForDeliveryMan/BgPurpose';
-import Story from 'components/organisms/Works/ForDeliveryMan/Story';
-import Process from 'components/organisms/Works/ForDeliveryMan/Process';
-import Murmur from 'components/organisms/Works/ForDeliveryMan/Murmur';
+import Mv from 'components/organisms/tmpWorks/ForDeliveryMan/Mv';
+import Visuals from 'components/organisms/tmpWorks/ForDeliveryMan/Visuals';
+import Target from 'components/organisms/tmpWorks/ForDeliveryMan/Target';
+import BgPurpose from 'components/organisms/tmpWorks/ForDeliveryMan/BgPurpose';
+import Story from 'components/organisms/tmpWorks/ForDeliveryMan/Story';
+import Process from 'components/organisms/tmpWorks/ForDeliveryMan/Process';
+import Murmur from 'components/organisms/tmpWorks/ForDeliveryMan/Murmur';
 
 const ForDeliveryManComponent: React.FC = () => {
   return (
