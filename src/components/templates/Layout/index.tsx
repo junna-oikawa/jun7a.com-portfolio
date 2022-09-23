64612ac9d0c8ed4b869b27099c0c0f854c3eee0a
junna-oikawa <jnn.y326@gmail.com>
2022-09-23 13:41:51 +0900

    [wip] ファイル名修正

--- src/components/templates/Layout/index.tsx
+++ src/components/templates/Layout/index.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react';
 import Style from './style.module.scss';
 import HeadComponent from 'components/atoms/HeadComponent';
-import Footer from 'components/organisms/common/Footer';
+import Footer from 'components/organisms/common/tmpFooter';
 import FooterNav from 'components/organisms/common/FooterNav';
-import Header from 'components/organisms/common/Header';
+import Header from 'components/organisms/common/tmpHeader';
 
 interface Props {
   children: ReactNode;
