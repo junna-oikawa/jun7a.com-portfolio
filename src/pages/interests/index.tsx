b28503058405ad65c679495fb12394e1ef508361
junna-oikawa <jnn.y326@gmail.com>
2022-09-23 13:45:55 +0900

    [fix] interestsページの削除

--- src/pages/interests/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next';
-import InterestsComponent from 'components/templates/Interests';
-
-const Interests: NextPage = () => {
-  return <InterestsComponent />;
-};
-
-export default Interests;
