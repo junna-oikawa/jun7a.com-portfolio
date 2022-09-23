764e57cc57520e17b1dcac34d48ab55c766b6cf7
junna-oikawa <43671757+junna-oikawa@users.noreply.github.com>
2022-09-23 14:25:20 +0900

    PF: works-hatomeの修正 (#18)
    * [add]ベースを作成
    
    * PF: topページ制作 (#2)
    
    * [create]topページpc
    
    * [add]mv-sp
    
    * [fix]mv-sp-文字サイズの調整
    
    * [add]top-works-sp
    
    * [add]top-about-sp
    
    * [create]common-footerNav
    
    * Feature/create hatomefriends (#3)
    
    * [create]work-hatomeFriends-mv
    
    * [add]work-mv レイアウト化
    
    * [create]works-hatome-sh
    
    * [add]word-hatome-layout
    
    * [create]works-hatome-descApp
    
    * [create]works-hatome-url
    
    * [fix] works-hatome-url フォントの調整
    
    * [create]works-hatome-target/purpose/bgs/langs
    
    * [fix]works-hatome-sh z-indexの調整
    
    * [create]works-hatome-youtube埋め込み
    
    * [create]works-hatome-development
    
    * [fix]works-hatome-sp時
    
    * [fix]全体-fontサイズの調整
    
    * [fix]ESLintエラー修正
    
    * [fix]works-hatome-swiper/cssのインポート
    
    * [fix]ESLint修正
    
    * [fix]Prettier設定修正
    
    * [fix]works-hatome-codeのscss (#5)
    
    * Feature/fix works hatome (#7)
    
    * [fix]works-hatome-codeのscss
    
    * [fix]works-hatome-target scssの修正
    
    * PF: サイト全体に関わる微修正 (#9)
    
    * [fix]index-works リンクを追加
    
    * [add]whole-基本色の追加
    
    * [add]header リンクの追加
    
    * PF: works/uniの作成 (#10)
    
    * [create]works-uni Mv
    
    * [create]works BG, PURPOSEレイアウト作成
    
    * [fix]works-uni visulas中央揃え修正
    
    * [create]works-uni-target
    
    * [create]works-uni uxマップ
    
    * [create]works-uni-process
    
    * [fix] works-uni-whole セクションごとの間隔調整
    
    * [fix]works-uni 微調整
    
    * PF: はとめふれんずページの修正 (#11)
    
    * [fix]ESLintの設定
    
    * [fix]works-hatome 微修正
    
    * [add]works-hatome-bgPurpose (#14)
    
    * PF: works/for_deliveryman ページの作成 (#16)
    
    * [create]works-fordeliveryman 下準備, MV
    
    * [create]works-fordeliveryman processまで
    
    * [fix]works-fordeliveryman sp時の対応
    
    * [fix]urlの修正
    
    * [fix]urlの修正
    
    * [fix] ESLintエラー修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [wip]ファイル名の修正
    
    * [fix] ファイル名修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [fix] インポートファイル名誤字の修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [wip] ファイル名修正
    
    * [fix] ファイル名修正
    
    * [fix] works-hatome セクション修正

--- src/components/organisms/Works/HatomeFriends/Target/index.tsx
+++ src/components/organisms/Works/HatomeFriends/Target/index.tsx
@@ -1,12 +1,10 @@
-import Layout from '../Layout';
-import Style from './style.module.scss';
+import Layout from '../../common/Layout';
+
 const Target: React.FC = () => {
   return (
-    <>
-      <Layout heading='Target' kana='ターゲット'>
-        <p>5~6歳を中心とした、就学前児童。</p>
-      </Layout>
-    </>
+    <Layout heading='Target' kana='ターゲット'>
+      <p>5~6歳を中心とした、就学前児童。</p>
+    </Layout>
   );
 };
 
