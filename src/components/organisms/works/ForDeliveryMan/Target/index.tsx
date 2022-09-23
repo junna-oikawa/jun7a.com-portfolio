706bd9c596536725afa9634f578362f30bfe748f
junna-oikawa <jnn.y326@gmail.com>
2022-09-23 11:55:09 +0900

    [create]works-fordeliveryman processまで

---
+++ src/components/organisms/works/ForDeliveryMan/Target/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../../common/Layout';
+
+const Target: React.FC = () => {
+  return (
+    <Layout heading='Target' kana='ターゲット'>
+      <p>宅配サービスを利用する人々</p>
+    </Layout>
+  );
+};
+
+export default Target;
--- src/components/templates/Works/ForDeliveryMan/index.tsx
+++ src/components/templates/Works/ForDeliveryMan/index.tsx
@@ -1,12 +1,21 @@
 import Style from './style.module.scss';
 import Layout from 'components/templates/Layout';
 import Mv from 'components/organisms/works/ForDeliveryMan/Mv';
+import Visuals from 'components/organisms/works/ForDeliveryMan/Visuals';
+import Target from 'components/organisms/works/ForDeliveryMan/Target';
+import BgPurpose from 'components/organisms/works/ForDeliveryMan/BgPurpose';
+import Story from 'components/organisms/works/ForDeliveryMan/Story';
 
 const ForDeliveryManComponent: React.FC = () => {
   return (
     <Layout>
       <Mv />
-      <div className={Style.width}></div>
+      <div className={Style.width}>
+        <Visuals />
+        <Target />
+        <BgPurpose />
+        <Story />
+      </div>
     </Layout>
   );
 };
